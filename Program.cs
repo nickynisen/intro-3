3e4738d236cc6cd03a36ad8399c180a71cb23105
04nini09 <04nini09@5CG21516C8>
2023-10-13 12:09:41 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using intro_3;
+
+
+Solution sol = new Solution();
+sol.Uppgift_1C();
---
+++ Solution.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace intro_3
+{ 
+    internal class Solution
+    {
+        public void Uppgift_1A()
+        {
+
+            Console.WriteLine("hej vad är ditt namn?");
+            Console.WriteLine("");
+            string namn = Console.ReadLine();
+            Console.ReadLine();
+            Console.WriteLine("hej och välkommen " + namn + "!");
+            Console.WriteLine();
+            double bredd = 9.6;
+            double höjd = 5.4;
+            double area;
+            area = bredd * höjd;
+            Console.WriteLine("rektangel");
+            Console.WriteLine("breed:" + bredd);
+            Console.WriteLine("höjd: " + höjd);
+            Console.WriteLine("area: " + area);
+            Console.WriteLine();
+        }
+        public void Uppgift_1C() { 
+            //Fråga om tal
+            Console.WriteLine("skriv in bredd: ");
+            // läs in tal som text
+            String text1 = Console.ReadLine();
+            // omvandla text till decimaltal
+            double bredd = Convert.ToDouble(text1);
+            Console.WriteLine("skriv in Höjd: ");
+            // läs in tal som text
+            String text2 = Console.ReadLine();
+            // omvandla text till decimaltal
+            double höjd = Convert.ToDouble(text2);
+            Console.WriteLine("Triangel");
+            Console.WriteLine("Höjd: " + höjd);
+            Console.WriteLine("Bredd: " + bredd);
+            Console.WriteLine("Area: " + bredd * höjd / 2);
+        }
+        public void Uppgift_1D()
+        {
+
+        }
+    }
+}
